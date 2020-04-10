11eff73175606501ecbafd1dbfa46b98c8460ffc
clownfresh <olof@clownfish.xyz>
2020-04-10 11:54:51 +0200

    readme and some fixes

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-**modParser.js**
-A small js library to parse mod files
+# modParser.js
+A small js library to parse mod files.
+
+## usage
+Everything is contained in the global `modParser` variable. 
+Read [this](https://greg-kennedy.com/tracker/modformat.html) for a more in-depth
+explanation on how to use all this.
+
+### methods
+`async parsefile(file)`  --  parses a mod-file and returns a mod-object.
+
+`parse string(string)` --  parses a binary string and returns a mod-object.
+
+
+### mod-object
+`name: String`  --  the name of the mod file.
+
+`samples: []`  --  contains all samples.
+
+`patterns: []`  --  contains all patterns.
+
+`patternOrder: [num]`  --  the order to play the patterns in.
+
+### sample
+`length: num`  --  how many bytes the sample contains.
+
+`data: []`  --  data for the sample. 
+
+### pattern
+A pattern is a two dimensional array. Rows indicate position 
+and columns indicate channel. It is filled with channel objects.
+
+### channel 
+`period: String`  --  period of the channel, i.e. pitch, in the form of strings like C4 or C3.
+
+`periodNum: Num`  --  the numerical representation of the period.
+
+`sample: Num`  --  which sample should be played
+
+`effect1: Num`  --  the effect the sample should be played with. first nibble
+
+`effect2: Num `  --  details about the effect. second nibble
+
+`effect2: Num `  --  details about the effect. third nibble
--- modParser.js
+++ modParser.js
@@ -1,4 +1,4 @@
-//TODO: implement samples, chances are I won't
+//TODO: implement samples
 
 let modParser; //global variable that contains the library
 
@@ -18,17 +18,31 @@ let modParser; //global variable that contains the library
   }
 
   let periods = {
+    1712: "C0", 1616: "C#0", 1525: "D0", 1440: "D#0", 1357: "E0", 1281: "F0",
+    1209: "F#0", 1141: "G0", 1077: "G#0", 1017: "A0", 961: "A#0", 907: "B0",
     856: "C1", 808: "C#1", 762: "D1", 720: "D#1", 678: "E1", 640: "F1",
     604: "F#1", 570: "G1", 538: "G#1", 508: "A1", 480: "A#1", 453: "B1",
     428: "C2", 404: "C#2", 381: "D2", 360: "D#2", 339: "E2", 320: "F2",
     302: "F#2", 285: "G2", 269: "G#2", 254: "A2", 240: "A#2", 226: "B2",
     214: "C3", 202: "C#3", 190: "D3", 180: "D#3", 170: "E3", 160: "F3",
-    151: "F#3", 143: "G3", 135: "G#3", 127: "A3", 120: "A#3", 113: "B3"
+    151: "F#3", 143: "G3", 135: "G#3", 127: "A3", 120: "A#3", 113: "B3",
+    107: "C4", 101: "C#4", 95: "D4", 90: "D#4", 85: "E4", 80: "F4", 
+    76: "F#4", 71: "G3", 67: "G#3", 64: "A3", 60: "A#3", 57: "B3"
   }
 
+  function readFileWithPromise(file) {
+    return new Promise( (resolve, reject) => {
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      }
+      reader.readAsBinaryString(file);
+    });
+  }
 
   function getMax(arr) {
-    let max = -Infinity
+    let max = -Infinity;
     for (let i = 0; i < arr.length; i++) {
       if (arr[i] > max) {
         max = arr[i];
@@ -39,7 +53,7 @@ let modParser; //global variable that contains the library
   }
 
   function getTrimmedLength(arr) {
-    let lastValue = 0
+    let lastValue = 0;
     for (let i = 0; i < arr.length; i++) {
       if (arr[i]) {
         lastValue = i;
@@ -53,7 +67,7 @@ let modParser; //global variable that contains the library
     parseString(source) {
       let mod = new Mod();
       let reader = new Reader(source);
-      mod.name = reader.read(20);
+      mod.name = reader.read(20).replace(/\u0000/g, '');
 
       //read M.K. Byte to get channels
       reader.seek(1080);
@@ -67,7 +81,7 @@ let modParser; //global variable that contains the library
       //read sample data
       for (let i = 0; i < samplesLength; i++) {
         mod.samples[i] = {};
-        mod.samples[i].name = reader.read(22);
+        mod.samples[i].name = reader.read(22).replace(/\u0000/g, '');
         mod.samples[i].sampleLength = reader.readByteNum() << 8 +
           reader.readByteNum();
         mod.samples[i].nibble = reader.readByteNum();
@@ -114,7 +128,9 @@ let modParser; //global variable that contains the library
 
             mod.patterns[pattern][position][channel] = {
               periodNum: ((byte1 & 0x0F) << 8) | byte2,
-              effect: ((byte3 & 0x0F) << 8) | byte4,
+              effect1: byte3 & 0x0F,
+              effect2: (byte4 & 0xF0) >> 4,
+              effect3: (byte4 & 0x0F),
               sample: (byte1 & 0xF0) | ((byte3 & 0xF0) >> 4)
             };
 
@@ -129,23 +145,23 @@ let modParser; //global variable that contains the library
         }
         //set length to last non zero position
         mod.patterns[pattern].length = lastPosition;
-      }  //  end loop through patterns
+      }//end loop through patterns
 
       return mod;
     }
-
+    
     async parseFile(file) {
-      let reader = new FileReader();
-      let source = reader.readAsBinaryString(file);
+      let string = await readFileWithPromise(file);
+      return this.parseString(string);
     }
   }
 
   class Mod {
     constructor() {
       this.name;
-      this.channels;
-      this.samples = []
-      this.patterns = []
+      this.samples = [];
+      this.patterns = [];
+      this.patternOrder = [];
     }
   }
 
